f6f5bad3f803704df3f7925497ee0fc71f9b5fb1
bitencourtjohann@gmail.com <bitencourtjohann@gmail.com>
2023-05-20 04:48:14 -0300

    initial commit

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
-console.log("running")
+const express = require('express')
+const app = express()
+
+
+app.get('/',(req,res)=>{
+  res.send('aaaaaaaa')
+})
+
+app.listen(3000,()=>{
+  console.log('running')
+})
